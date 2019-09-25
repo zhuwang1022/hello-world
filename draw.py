4095f8c7e50a58a414e38e7946ae6acc667d4030
Zhu Wang <juliezhuwang@gmail.com>
2019-09-25 12:34:05 -0400

    add two files

--- draw.py
+++ draw.py
@@ -1,11 +1,11 @@
-import turtle 
+import turtle
 
 painter = turtle.Turtle()
 
 painter.pensize(20)
 
 for _ in range(18):
-	painter.forward(100)
-	painter.left(100)
+    painter.forward(100)
+    painter.left(100)
 
 turtle.done()
--- triangle.py
+++ triangle.py
@@ -1,9 +1,9 @@
-import turtle 
+import turtle
 
 painter = turtle.Turtle()
 
 for _ in range(3):
-	painter.forward(100)
-	painter.left(120)
+    painter.forward(100)
+    painter.left(120)
 
 turtle.done()
